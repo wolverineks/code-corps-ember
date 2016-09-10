c97c743bd153b6f6a1e76e1892d21e2d70457061
wenincode <twendlan@ualberta.ca>
2016-09-10 00:48:03 -0600

    update project using ember-cli@2.8.0

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'code-corps-ember',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -135,7 +135,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
